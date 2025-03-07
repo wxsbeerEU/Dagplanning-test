76ede8cb1798a59c53e0abffbd2d541ae1440e03
wxsbeerEU <ninjo.de.grote@gmail.com>
2025-03-07 07:31:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const schedule = [
     { time: '22:00', activity: 'Niet meer douchen' },
     { time: '22:00', activity: 'Slapen gaan 1e graad roze bandje' },
     { time: '22:15', activity: 'Slapen gaan 2e graad blauw bandje' },
-    { time: '22:30', activity: 'Slapen gaan 3e graad groen bandje' }
+    { time: '22:30', activity: 'Slapen gaan 3e graad groen bandje' },
     { time: '23:00', activity: 'Lichten uit en in bed blijven' }
 ];
 
